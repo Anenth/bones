a81ef380da1a243b5e8dfa7283e1c409e6705bc5
Anenth <anenthvishnu@gmail.com>
2015-04-27 23:11:46 +0530

    sidebar cleanup

--- sidebar.php
+++ sidebar.php
@@ -1,21 +1,9 @@
-				<div id="sidebar1" class="sidebar m-all t-1of3 d-2of7 last-col cf" role="complementary">
+<div id="sidebar1" class="sidebar m-all t-1of3 d-2of7 last-col cf" role="complementary">
 
-					<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
+  <?php query_posts('showposts=3'); ?>
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <a href="<?php the_permalink() ?>"><?php the_title() ?></a><br />
+  <?php endwhile; endif; ?>
 
-						<?php dynamic_sidebar( 'sidebar1' ); ?>
 
-					<?php else : ?>
-
-						<?php
-							/*
-							 * This content shows up if there are no widgets defined in the backend.
-							*/
-						?>
-
-						<div class="no-widgets">
-							<p><?php _e( 'This is a widget ready area. Add some and they will appear here.', 'bonestheme' );  ?></p>
-						</div>
-
-					<?php endif; ?>
-
-				</div>
+ </div>
